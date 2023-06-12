bdcb5dcae1bd6c4fa96d34bd4cd3136b70443484
wijnandb <wijnandb@retta.nl>
2023-06-12 22:35:34 +0200

    Add startlists current races

--- generate_ranking/current_races.py
+++ generate_ranking/current_races.py
@@ -8,7 +8,7 @@
 """
 
 
-import process_files
+import process_files, start_list
 
 import datetime
 
@@ -29,10 +29,23 @@ for c in calendar_with_startdate:
     calendar_with_dates.append(c)
     #print(calendar_with_dates[-1])
 
+
+def create_html_file(c):
+    # generate the HTML output
+    output = '---\nlayout: default\ntitle: Startlijst '+ c[2] +' ('+ c[4] +')\npermalink: /startlist/races/'+ c[3] +'/\n'
+    output += 'race_id: '+ c[3] +'\n---\n\n'
+    output += '{% include startlist.html %}\n\n'
+    output += '<a href="https://firstcycling.com/race.php?r='+ c[3] +'&y=2023&k=8" target="_new" title="Bekijk op FirstCycling.com">Bekijk op FirstCycling</a>\n\n'
+
+
+    # write the output to an HTML file
+    with open('startlist/races/'+ c[3] +'.html', 'w') as htmlfile:
+        htmlfile.write(output)
+
 def is_current_race(race):
     # These are the races that are on this week
     if race[0].isocalendar().week == currentweek:
-        # print(f"Eendaagse koers of start meerdaagse koerss {race}")
+        # print(f"Eendaagse koers of start meerdaagse koers {race}")
         return True
     elif race[1] != "":
         if race[1].isocalendar().week == currentweek:
@@ -47,8 +60,9 @@ current_races = [['startdate','enddate','race','FC_race_id','category','points',
 for race in calendar_with_dates:
     if is_current_race(race):
         current_races.append(race)
-    
+        start_list.get_riders(race[3], '2023')
+        create_html_file(race)
+        
 process_files.write_csv_file("current_races.csv", current_races)
-# Now we also want to have the races that have already started but aren't finished yet
-# that means 
+
 
